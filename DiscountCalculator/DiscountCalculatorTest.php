394e6ddd70074fd4fa3154fc5dae1bb6ed9991cd
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-06-30 13:55:56 -0300

    file DiscountCalculatorTest.php to test discountCalculator

---
+++ DiscountCalculator/DiscountCalculatorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class DiscountCalculatorTeste {
+
+    public function ShouldApplay_WhenValueIsAboveTheMinimum($value){
+        $discountCalculator = new DiscountCalculator();
+
+        $totalValue = 130;
+        $totalWithDiscount = $discountCalculator -> apply($totalValue);
+
+        $expectedValue = 110;
+        $this->assertEquals($expectedValue, $totalWithDiscount);
+
+
+    }
+
+    public function assertEquals($expectedValue, $actualValue){
+        if($expectedValue !== $actualValue){
+            $message = 'Expected: '.$expectedValue.' but got: '.$actualValue;
+            throw new \Exception($message);
+        }
+        echo 'Test passed<br>';
+    }
+}
